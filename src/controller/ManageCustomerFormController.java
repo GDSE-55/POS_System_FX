4adeccf603756d49ef4341a957c7bc2f41f9a50f
DilkiC <dilkicooray97@gmail.com>
2021-10-09 10:25:53 +0530

    'finished-cust-close-#1'

---
+++ src/controller/ManageCustomerFormController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableView;
+
+/**
+ * @author Dilki Cooray <dilkicooray97@gmail.com>
+ * since 10/7/2021
+ */
+public class ManageCustomerFormController {
+    public JFXTextField txtCustomerId;
+    public JFXTextField txtCustomerName;
+    public JFXTextField txtCustomerAddress;
+    public TableView tblCustomers;
+
+    public void btnSave_OnAction(ActionEvent actionEvent) {
+
+    }
+}
